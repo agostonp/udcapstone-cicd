f601cb4fbe097b937c0b9aa03d34e98a18c8c312
Agoston Pajzs <agostonp@gmail.com>
2020-05-15 09:15:07 +0000

    eksctl version of kube cluster creation - still has permission issues

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -11,3 +11,11 @@ sudo apt-get install -y tidy
 # install hadolint
 sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.17.6/hadolint-Linux-x86_64
 sudo chmod +x /bin/hadolint
+
+# install eksctl
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
+
+# verify that kubectl is installed
+kubectl version --short --client
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
+regioncode="eu-central-1"
 
-# This tags and uploads an image to Docker Hub
+# Create an Amazon EKS cluster with Fargate support
+eksctl version
+eksctl create cluster --name udcapstone --region "eu-central-1" --fargate
 
-# Step 1:
+# configure kubectl to use the same IAM Role
+aws --region "eu-central-1" eks update-kubeconfig --name udcapstone --role-arn arn:aws:iam::857339242870:role/eksctl-udcapstone-cluster-FargatePodExecutionRole-1VUQRGJXM8F88
+
 # This is your Docker ID/path
 dockerpath="agostonp/udproj-kubernetes:uploaded"
 
-# Step 2
 # Run the Docker Hub container with kubernetes
 #kubectl run udprojkubedemo --image="agostonp/udproj-kubernetes:uploaded" --port=8000 --generator="run-pod/v1"
 kubectl run udprojkubedemo --image=$dockerpath --port=8000 --generator=run-pod/v1
 
-# Step 3:
 # List kubernetes pods
 kubectl get pods
 
-# Step 4:
 # Forward the container port to a host
 # Listen on port 8000 locally, forwarding to 80 in the pod
 kubectl port-forward pod/udprojkubedemo 8000:80
